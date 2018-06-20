065ecdc04df546c7f87242d0932f343f6134a86c
Martian <martian2lee@163.com>
2018-06-20 16:01:24 +0400

    chore

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
 
-exports.homePage = (req, res) => {
-  res.render('index')
-}
-
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' })
 }
