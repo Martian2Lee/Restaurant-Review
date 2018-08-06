3b911c18021b7f96d5e9ab70439e2a6f0d69eb79
Martian <martian2lee@163.com>
2018-08-06 23:39:12 +0400

    Routing and Templating Single Stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -77,3 +77,9 @@ exports.updateStore = async (req, res) => {
   res.redirect(`/stores/${store._id}/edit`)
   // redirect them to the store
 }
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug })
+  if (!store) return next()
+  res.render('store', { store, title: store.name })
+}
