218d7e1dc9b8f588c186b1e0d10d63b644a9b249
Martian <martian2lee@163.com>
2018-10-17 21:43:09 +0400

    Displaying our Hearted Stores

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,11 @@ router.post(
   catchErrors(authController.update)
 )
 router.get('/map', storeController.mapPage)
+router.get(
+  '/hearts',
+  authController.isLoggedIn,
+  catchErrors(storeController.getHearts)
+)
 
 /*
   API
